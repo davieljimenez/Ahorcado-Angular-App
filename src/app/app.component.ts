fc2d7018604d625e26470ccc408888082bfd8d9c
Daviel Jimenez <Daviel050399@gmail.com>
2020-06-08 00:13:04 -0400

    Agregar letras desde TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ahorcado';
+  
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+  'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+  'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+  constructor() {
+    console.log(`Se acaba de crear el app component`)
+  }
 }
