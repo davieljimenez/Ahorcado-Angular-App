ef1210352f604fa0fb94195377b3b26edee0ffe0
Daviel Jimenez <Daviel050399@gmail.com>
2020-06-11 21:32:33 -0400

    Rayas palabra oculta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+
+  palabra = 'AGUA';
+  palabraOculta = ''
   
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
   'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
   'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   
   constructor() {
-    console.log(`Se acaba de crear el app component`)
+    this.palabraOculta = " _".repeat(this.palabra.length);
+
   }
 }
